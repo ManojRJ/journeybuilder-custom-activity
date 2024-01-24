645c962a2fc531486c027879627545a0de2a9cf5
ManojRJ <manoj92rj@gmail.com>
2024-01-24 10:49:52 +0530

    initial commit

--- routes/activity.js
+++ routes/activity.js
@@ -11,8 +11,9 @@ const logger = require("../utils/logger");
  */
 exports.execute = async (req, res) => {
   // decode data
+  console.log("activity js JWT input", req.body);
   const data = JWT(req.body);
-
+  console.log("activity js JWT output", data);
   logger.info(data);
 
   try {
