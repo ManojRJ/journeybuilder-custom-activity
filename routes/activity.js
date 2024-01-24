967133fd4fe152527f45f25f8416ab79cf993eff
ManojRJ <manoj92rj@gmail.com>
2024-01-24 19:12:20 +0530

    acc

--- routes/activity.js
+++ routes/activity.js
@@ -2,13 +2,32 @@ const { v1: Uuidv1 } = require("uuid");
 const JWT = require("../utils/jwtDecoder");
 const SFClient = require("../utils/sfmc-client");
 const logger = require("../utils/logger");
+const jwts = require("jsonwebtoken");
 
 /**
  * The Journey Builder calls this method for each contact processed by the journey.
  * @param req
  * @param res
  * @returns {Promise<void>}
+ *
+ *
+ *
  */
+
+const secretKey =
+  "QecXGync1dl34H_Ro8ZKnveYC81ODv-FAtfwsjHwltYMXPH3y8XQx5mlTQWk638Lg8RPwDhlIyY-GqOXS9ykvAzzdviYyS-Nd1Rd9ESeZ-iFLTntoOFnq25jhLmfCKO7ew53k9hOIRuO2QhHhecAlAXX14p-aw_cz7hdKJGBrIOk-04Zbs4kUvjMS4aIcdJkkAr6gaF9k0dTEUAiwg8W4LE1DJHfZLWNyb85iYBjY4Kkw5vDBKskiLkUdiBbGw2";
+
+const token =
+  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.ew0KICAiaW5Bcmd1bWVudHMiOiBbDQogICAgew0KICAgICAgImNvbnRhY3RLZXkiOiAibWozIiwNCiAgICAgICJEcm9wZG93bk9wdGlvbnMiOiAiam91cm5leUVudHJ5IiwNCiAgICAgICJUZXh0IjogIkhlbGxvIg0KICAgIH0NCiAgXSwNCiAgImFjdGl2aXR5T2JqZWN0SUQiOiAiZDY5NjI0ZjYtMzhkYi00N2Y4LTg2M2MtZDBkNjYzNjgzZWI3IiwNCiAgImpvdXJuZXlJZCI6ICJlYTE1M2Y5My0xNWI5LTRhYmQtODBiMS01ZDgwOGUxMGFhZTciLA0KICAiYWN0aXZpdHlJZCI6ICJkNjk2MjRmNi0zOGRiLTQ3ZjgtODYzYy1kMGQ2NjM2ODNlYjciLA0KICAiZGVmaW5pdGlvbkluc3RhbmNlSWQiOiAiNzY2MTIyZmEtYmJkNS00MWEzLWE1MzktOWZjMjVhY2U5YWFjIiwNCiAgImFjdGl2aXR5SW5zdGFuY2VJZCI6ICI2MWQ3M2E2ZC00YTRkLTRmNzMtODJkZi02ODQwZTU5ODVhYzMiLA0KICAia2V5VmFsdWUiOiAibWozIiwNCiAgIm1vZGUiOiAwDQp9.eNcIHjLpNVgcT1Vr-aYa4nChaVZrpUwZgWH4cWXVVCk";
+
+jwts.verify(token, secretKey, (err, decoded) => {
+  if (err) {
+    console.error("Token verification failed:", err);
+  } else {
+    console.log("Token verified successfully:", decoded);
+  }
+});
+
 exports.execute = async (req, res) => {
   // decode data
   console.log("activity js JWT input", JWT);
