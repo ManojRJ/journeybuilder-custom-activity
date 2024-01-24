5e503b50f150c9da69314b1df19a97c46745611c
ManojRJ <manoj92rj@gmail.com>
2024-01-24 11:56:45 +0530

    decoder and app

--- utils/jwtDecoder.js
+++ utils/jwtDecoder.js
@@ -1,11 +1,13 @@
-const JWT = require('jsonwebtoken');
+const JWT = require("jsonwebtoken");
 
 module.exports = (body) => {
   if (!body) {
-    return new Error('invalid jwtdata');
+    return new Error("invalid jwtdata");
   }
+  console.log("JWT body", body.toString("utf8"));
+  console.log("JWT variable", process.env.JWT);
 
-  return JWT.verify(body.toString('utf8'), process.env.JWT, {
-    algorithm: 'HS256',
+  return JWT.verify(body.toString("utf8"), process.env.JWT, {
+    algorithm: "HS256",
   });
 };
