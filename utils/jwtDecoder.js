33e6f0ebfda5b13241e1397ba2fc8d3bfedfb7b5
ManojRJ <manoj92rj@gmail.com>
2024-01-24 12:15:59 +0530

    decoder

--- utils/jwtDecoder.js
+++ utils/jwtDecoder.js
@@ -4,7 +4,8 @@ module.exports = (body) => {
   if (!body) {
     return new Error("invalid jwtdata");
   }
-  console.log("JWT body", body.toString("utf8"));
+  console.log("JWT body", body.toString());
+  console.log("JWT body object", body);
   console.log("JWT variable", process.env.JWT);
 
   return JWT.verify(body.toString("utf8"), process.env.JWT, {
