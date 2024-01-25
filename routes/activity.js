a37bba56c7a3649c1b039e59e697d64791f91aa4
ManojRJ <manoj92rj@gmail.com>
2024-01-25 13:28:31 +0530

    new update

--- routes/activity.js
+++ routes/activity.js
@@ -14,23 +14,23 @@ const jwts = require("jsonwebtoken");
  *
  */
 
-const secretKey =
-  "QecXGync1dl34H_Ro8ZKnveYC81ODv-FAtfwsjHwltYMXPH3y8XQx5mlTQWk638Lg8RPwDhlIyY-GqOXS9ykvAzzdviYyS-Nd1Rd9ESeZ-iFLTntoOFnq25jhLmfCKO7ew53k9hOIRuO2QhHhecAlAXX14p-aw_cz7hdKJGBrIOk-04Zbs4kUvjMS4aIcdJkkAr6gaF9k0dTEUAiwg8W4LE1DJHfZLWNyb85iYBjY4Kkw5vDBKskiLkUdiBbGw2";
+// const secretKey =
+//   "QecXGync1dl34H_Ro8ZKnveYC81ODv-FAtfwsjHwltYMXPH3y8XQx5mlTQWk638Lg8RPwDhlIyY-GqOXS9ykvAzzdviYyS-Nd1Rd9ESeZ-iFLTntoOFnq25jhLmfCKO7ew53k9hOIRuO2QhHhecAlAXX14p-aw_cz7hdKJGBrIOk-04Zbs4kUvjMS4aIcdJkkAr6gaF9k0dTEUAiwg8W4LE1DJHfZLWNyb85iYBjY4Kkw5vDBKskiLkUdiBbGw2";
 
-const token =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpbkFyZ3VtZW50cyI6W3siY29udGFjdEtleSI6Im1qMyIsIkRyb3Bkb3duT3B0aW9ucyI6ImpvdXJuZXlFbnRyeSIsIlRleHQiOiJIZWxsbyJ9XSwiYWN0aXZpdHlPYmplY3RJRCI6ImQ2OTYyNGY2LTM4ZGItNDdmOC04NjNjLWQwZDY2MzY4M2ViNyIsImpvdXJuZXlJZCI6ImVhMTUzZjkzLTE1YjktNGFiZC04MGIxLTVkODA4ZTEwYWFlNyIsImFjdGl2aXR5SWQiOiJkNjk2MjRmNi0zOGRiLTQ3ZjgtODYzYy1kMGQ2NjM2ODNlYjciLCJkZWZpbml0aW9uSW5zdGFuY2VJZCI6Ijc2NjEyMmZhLWJiZDUtNDFhMy1hNTM5LTlmYzI1YWNlOWFhYyIsImFjdGl2aXR5SW5zdGFuY2VJZCI6IjYxZDczYTZkLTRhNGQtNGY3My04MmRmLTY4NDBlNTk4NWFjMyIsImtleVZhbHVlIjoibWozIiwibW9kZSI6MH0.vPV4PA_iZcWDLCMr3WCCGMPeSR9Jw2Mud2ziqBrl1s8";
+// const token =
+//   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpbkFyZ3VtZW50cyI6W3siY29udGFjdEtleSI6Im1qMyIsIkRyb3Bkb3duT3B0aW9ucyI6ImpvdXJuZXlFbnRyeSIsIlRleHQiOiJIZWxsbyJ9XSwiYWN0aXZpdHlPYmplY3RJRCI6ImQ2OTYyNGY2LTM4ZGItNDdmOC04NjNjLWQwZDY2MzY4M2ViNyIsImpvdXJuZXlJZCI6ImVhMTUzZjkzLTE1YjktNGFiZC04MGIxLTVkODA4ZTEwYWFlNyIsImFjdGl2aXR5SWQiOiJkNjk2MjRmNi0zOGRiLTQ3ZjgtODYzYy1kMGQ2NjM2ODNlYjciLCJkZWZpbml0aW9uSW5zdGFuY2VJZCI6Ijc2NjEyMmZhLWJiZDUtNDFhMy1hNTM5LTlmYzI1YWNlOWFhYyIsImFjdGl2aXR5SW5zdGFuY2VJZCI6IjYxZDczYTZkLTRhNGQtNGY3My04MmRmLTY4NDBlNTk4NWFjMyIsImtleVZhbHVlIjoibWozIiwibW9kZSI6MH0.vPV4PA_iZcWDLCMr3WCCGMPeSR9Jw2Mud2ziqBrl1s8";
 
-jwts.verify(token, secretKey, (err, decoded) => {
-  if (err) {
-    console.error("Token verification failed:", err);
-  } else {
-    console.log("Token verified successfully:", decoded);
-  }
-});
+// jwts.verify(token, secretKey, (err, decoded) => {
+//   if (err) {
+//     console.error("Token verification failed:", err);
+//   } else {
+//     console.log("Token verified successfully:", decoded);
+//   }
+// });
 
 exports.execute = async (req, res) => {
   // decode data
-  console.log("activity js JWT input", JWT);
+  console.log("activity js JWT input", req.body);
   const data = JWT(req.body);
   console.log("activity js JWT output", data);
   logger.info(data);
